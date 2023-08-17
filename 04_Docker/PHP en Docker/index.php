73a8d76509e0850987b9c276b959beccf4477e30
PhoenixContactArgentina <139789794+PhoenixContactArgentina@users.noreply.github.com>
2023-08-17 15:58:09 -0300

    Add files via upload

---
+++ 04_Docker/PHP en Docker/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+	<title>PHP en PLCnext con Docker</title>
+</head>
+<body>
+	
+<h1>
+<?php echo "Hello PLCnext with docker"; ?> 
+</h1>	
+
+</body>
+</html>
+
+
+
+
+
+
+
